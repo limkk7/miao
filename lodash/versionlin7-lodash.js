b2e7af4c293d9b958dd0b68468b921a4d9dce163
linpeng <versionlin@foxmail.com>
2019-07-13 12:41:36 +0800

    chunk

--- lodash/versionlin7-lodash.js
+++ lodash/versionlin7-lodash.js
@@ -1,4 +1,11 @@
 var versionlin7 = {
+  chunk: function(ary, size = 1) {
+    let array = []
+    for (let i = 0; i < ary.length; i += size){
+      array.push(ary.slice(i, i + size))
+    }
+    return array
+  },
   compact: function(ary) {
     return ary.filter(it => it)
   },
