bcec6e548a50ea31d40e51172c7e8337e1a343d9
linpeng <versionlin@foxmail.com>
2019-07-21 20:04:19 +0800

    differenceBy

--- lodash/versionlin7-lodash.js
+++ lodash/versionlin7-lodash.js
@@ -9,11 +9,26 @@ var versionlin7 = {
   compact: function(ary) {
     return ary.filter(it => it)
   },
+  /**
+   * [difference description]
+   *Creates an array of `array` values not included in *the other given arrays
+   * 
+   * @param   {Array}  ary       [ary description]
+   * @param   {...Array}  ...value  [...value description]
+   *
+   * @return  {Array}            [Returns the new array of filtered values]
+   */
   difference: function(ary, ...value) {
     var array = ary.slice()
     for (let val of value) {
         array = array.filter(n => !val.includes(n))
     }
     return array
+  },
+  differenceBy: function(array, ...values, action) {
+    let ary = array.slice()
+    for(let val of values) {
+      ary = ary.filter(n => !val.includes(action(n)))
+    }
   },
 }
